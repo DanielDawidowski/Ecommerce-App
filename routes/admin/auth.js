3a63712232392a41eac216fb335a5b56f95a0325
Daniel Dawidowski <dvds1987@gmail.com>
2019-12-06 10:16:53 +0100

    Extracting Validation Chains

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,6 +3,7 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validatos')
 
 const router = express.Router()
 
@@ -14,46 +15,17 @@ router.get('/signup', (req, res) => {
 router.post(
     '/signup', 
     [
-        check('email')
-            .trim()
-            .normalizeEmail()
-            .isEmail()
-            .withMessage('Must be a valid email')
-            .custom(async (email) => {
-                const existingUser = await usersRepo.getOneBy({ email })
-                if (existingUser) {
-                   throw new Error('Email in use')
-                }
-            }),
-        check('password')
-            .trim()
-            .isLength({ min: 4, max: 20 })
-            .withMessage('Must be berween 4 to 20 characters'),
-        check('passwordConfirmation')
-            .trim()
-            .isLength({ min: 4, max: 20 })
-            .withMessage('Must be berween 4 to 20 characters')
-            .custom((passwordConfirmation, { req }) => {
-                if (passwordConfirmation !== req.body.password) {
-                    throw new Error('Passwords must match')
-                }
-            })
+        requireEmail,
+        requirePassword,
+        requirePasswordConfirmation
     ], 
     async (req, res) => {
         const errors = validationResult(req)
         console.log(errors)
+
         const { email, password, passwordConfirmation } = req.body;
-
-        
-
-        if (password !== passwordConfirmation) {
-            return res.send("Password must match")
-        }
-
-        // Create a user in our user repo to represent this person 
         const user = await usersRepo.create({ email, password });
 
-        // Store the id of that user inside the users cookie
         req.session.userId = user.id 
 
         res.send('Account created !')
